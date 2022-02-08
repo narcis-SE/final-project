bd93ef1a882bcc3d9963d876f0a892aaed38af60
narcis <narcissprecic@gmail.com>
2022-02-08 14:56:59 -0500

    Working new API integration

--- nbafront/src/services/StandingsServices.ts
+++ nbafront/src/services/StandingsServices.ts
@@ -4,10 +4,18 @@ import {Standings} from '../models/Standings'
 const apiKey:string = process.env.REACT_APP_STANDINGS_API_NBA_KEY || "";
 
 export function fetchStandings():Promise<Standings[]>{
-    return axios.get("https://api-nba-v1.p.rapidapi.com/standings/standard/2021/?rapidapi-key=",{
-        params:{
-            'rapidapi-key': apiKey
+    
+    return axios.get("https://api-basketball.p.rapidapi.com/standings",{
+        params:{league: '12', season: '2021-2022'},
+        headers:{
+            'x-rapidapi-host': 'api-basketball.p.rapidapi.com',
+            'rapidapi-key': apiKey 
         }
     })
-    .then (response=> response.data)
+    .then(response=> response.data)
+
 }
+//   var cron = require('node-cron');
+//     cron.schedule('1 * * * *', () => {
+//         console.log("hi");
+//     });
