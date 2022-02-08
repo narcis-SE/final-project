199ca43848dfcd1f3d41d03b77bfec92350f793a
james <jamesedwarddevine@gmail.com>
2022-02-08 09:56:47 -0500

    added scheduler

--- nbafront/src/App.tsx
+++ nbafront/src/App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Main } from './components/Main';
+import { fetchStandings } from './services/StandingsServices';
+import { Standings } from './models/Standings';
+
+
 
 function App() {
   return (
--- nbafront/src/components/Main.tsx
+++ nbafront/src/components/Main.tsx
@@ -4,6 +4,8 @@ import { News } from "../models/News";
 import { Header } from "./Header";
 import { ArticleList } from "./ArticleList";
 import Generator from "./generator";
+import { Standings } from "../models/Standings";
+import { fetchStandings } from "../services/StandingsServices";
 
 
 interface NewsProp{
@@ -14,6 +16,17 @@ interface NewsProp{
 
 export const Main = () => {
     const[articles, setArticle] = useState<News[]>([]);
+    // const[standings, setStandings] = useState<Standings[]>([]);
+
+    // var cron = require('node-cron');
+    
+    // cron.schedule('*/5 * * * *', () => {
+    //     fetchStandings().then(
+    //         standings=>setStandings(standings)
+    //     )
+    //     console.log(standings);
+    // });
+
 
     useEffect(()=>{
         fetchNbaNews().then(
