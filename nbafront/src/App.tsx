8d816426d7b50774ef4651edcf166d0c043788ad
narcis <narcissprecic@gmail.com>
2022-02-10 16:12:56 -0500

    fixed the styles of table

--- nbafront/src/App.tsx
+++ nbafront/src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Main } from './components/Main';
 import { Standings } from './models/Standings';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 
 
--- nbafront/src/components/StandingsList.tsx
+++ nbafront/src/components/StandingsList.tsx
@@ -1,4 +1,5 @@
 import { Standings } from "../models/Standings";
+import "./StandingsList.css"
 import { Standing } from "./Standing";
 import Table from 'react-bootstrap/Table'
 export interface Prop{
@@ -10,9 +11,9 @@ export const StandingsList = ({standings}: Prop) =>{
         <div className="StandingList">
               <Table  hover variant="dark">
               <thead>
+              <h1>Western Conference</h1>
                  <tr>
                   <th> Position </th>
-                  <th> Conference </th>
                   <th> Team </th>
                   <th> Win/Loss</th>
                  </tr>
@@ -22,36 +23,34 @@ export const StandingsList = ({standings}: Prop) =>{
                 {standings.slice(0,7).map((standings,i) =>
                     <tr className="table-success">
                     <td>{standings.position}</td>
-                    <td>{standings.group.name}</td>
                     <td>{standings.team.name}</td>
-                    <td>{standings.games.win.total} / {standings.games.lose.total}</td>
+                    <td>{standings.games.win.total} - {standings.games.lose.total}</td>
                     </tr>
                     )}
                 {standings.slice(7,9).map((standings,i) =>
                     <tr className="table-warning">
                     <td>{standings.position}</td>
-                    <td>{standings.group.name}</td>
                     <td>{standings.team.name}</td>
-                    <td>{standings.games.win.total} / {standings.games.lose.total}</td>
+                    <td>{standings.games.win.total} - {standings.games.lose.total}</td>
                     </tr>
                     )}
 
                 {standings.slice(9,15).map((standings,i) =>
                     <tr className="table-danger">
                     <td>{standings.position}</td>
-                    <td>{standings.group.name}</td>
                     <td>{standings.team.name}</td>
-                    <td>{standings.games.win.total} / {standings.games.lose.total}</td>
+                    <td>{standings.games.win.total} - {standings.games.lose.total}</td>
                     </tr>
                     )}
                 </tbody>
               </Table>
-
+              
               <Table hover variant="dark">
+              
               <thead>
+              <h1>Eastern Conference</h1>
                  <tr>
                   <th> Position </th>
-                  <th> Conference </th>
                   <th> Team </th>
                   <th> Win/Loss</th>
                  </tr>
@@ -60,26 +59,23 @@ export const StandingsList = ({standings}: Prop) =>{
                 {standings.slice(15,22).map((standings,i) =>
                     <tr className="table-success">
                     <td>{standings.position}</td>
-                    <td>{standings.group.name}</td>
                     <td>{standings.team.name}</td>
-                    <td>{standings.games.win.total} / {standings.games.lose.total}</td>
+                    <td>{standings.games.win.total} - {standings.games.lose.total}</td>
                     </tr>
                     )}
                 {standings.slice(22,24).map((standings,i) =>
                     <tr className="table-warning">
                     <td>{standings.position}</td>
-                    <td>{standings.group.name}</td>
                     <td>{standings.team.name}</td>
-                    <td>{standings.games.win.total} / {standings.games.lose.total}</td>
+                    <td>{standings.games.win.total} - {standings.games.lose.total}</td>
                     </tr>
                     )}
 
-                {standings.slice(25,30).map((standings,i) =>
+                {standings.slice(24,30).map((standings,i) =>
                     <tr className="table-danger">
                     <td>{standings.position}</td>
-                    <td>{standings.group.name}</td>
                     <td>{standings.team.name}</td>
-                    <td>{standings.games.win.total} / {standings.games.lose.total}</td>
+                    <td>{standings.games.win.total} - {standings.games.lose.total}</td>
                     </tr>
                     )}
                 </tbody>
